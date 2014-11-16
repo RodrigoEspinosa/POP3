c02cc5b4ad70d1c0e322f5f46239bf0b042fde9d
Rodrigo Espinosa <espinosacurbelo@gmail.com>
2014-11-16 21:44:35 -0200

    Feat(models): Added garden to House model

--- POP3/POP3/Models/House.cs
+++ POP3/POP3/Models/House.cs
@@ -4,14 +4,29 @@ using POP3;
 
 namespace POP3
 {
-    public class House: Property<House>
+    public class House : Property<House>
     {
-        protected bool garage;
-        
-        public House(int bedrooms, int bathrooms, int years, int builtMeters, String adress1, String adress2, String number, bool forRent, bool forSale, int rentPrice, int sellPrice, INeighborhood neighborhood, bool garage)
+        private bool garage;
+        private bool garden;
+
+        public bool Garage
+        {
+            get { return garage; }
+            set { this.garage = value; }
+        }
+
+        public bool Garden
+        {
+            get { return garden; }
+            set { this.garden = value; }
+        }
+
+        public House(int bedrooms, int bathrooms, int years, int builtMeters, String adress1, String adress2,
+            String number, bool forRent, bool forSale, int rentPrice, int sellPrice, INeighborhood neighborhood, bool garage, bool garden=false)
 		    : base(bedrooms, bathrooms, years, builtMeters, adress1, adress2, number, forRent, forSale, rentPrice,sellPrice, neighborhood)
         {
             this.garage = garage;
+            this.garden = garden;
         }
     }
 }
